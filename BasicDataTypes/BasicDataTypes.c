a2c9fadab9bb5efbfdf7b3cd8ae76d8c09e2da5a
Abia Oelberg <data.chand@gmail.com>
2015-12-19 15:45:25 +0530

    C++ Headstart

---
+++ BasicDataTypes/BasicDataTypes.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int main() {
+	int x, y;
+	long long int z;
+	char w;
+	float v;
+	double u;
+	// Complete the code.
+	scanf("%d %d %lld %c %f %lf", &x, &y, &z, &w, &v, &u);
+	printf("%d\n%d\n%lld\n%c\n%f\n%lf", x, y, z, w, v, u);
+
+	return 0;
+}
